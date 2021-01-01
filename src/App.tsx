dd8c755afb3686b051d9b385752ce77534edb9d5
Nikolay Alexeev <kodi_kodi_kodi@mail.ru>
2021-01-01 22:41:26 +0300

    product card, fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import './scss/app.scss'
 import { Cart, Catalog } from './pages'
 import { Header } from './components'
+import axios from 'axios'
+
+export interface IItem {
+  id: number
+  imageUrl: string
+  name: string
+  types: number[]
+  sizes: number[]
+  price: number
+  category: number
+  rating: number
+}
+
+interface IApiGetItems {
+  pizzas: IItem[]
+}
 
 export const App: React.FC = () => {
+  const [items, setItems] = useState<IItem[]>([])
+
+  useEffect(() => {
+    axios
+      .get<IApiGetItems>('http://localhost:3000/db.json')
+      .then(({ data }) => setItems(data.pizzas))
+  }, [])
+
   return (
     <div className='wrapper'>
       <Header />
       <div className='content'>
         <Switch>
           <Route exact path='/'>
-            <Catalog />
+            <Catalog items={items} />
           </Route>
           <Route exact path='/cart'>
             <Cart />
--- src/utils/hooks/useSelectItem.ts
+++ src/utils/hooks/useSelectItem.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
-export const useSelectItem = () => {
-  const [activeItem, setActiveItem] = useState<number>(0)
+export const useSelectItem = (initialValue: number = 0) => {
+  const [activeItem, setActiveItem] = useState<number>(initialValue)
 
   const handleSelectItem = (index: number) => setActiveItem(index)
 
